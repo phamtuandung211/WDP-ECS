047fbbe624c2bff3a33e882bf5e1d4c6bac83bcb
phamtuandung211 <dp2110044@gmail.com>
2026-03-08 14:02:10 +0700

    fix admin dashboard

--- be/src/index.js
+++ be/src/index.js
@@ -43,7 +43,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes)
+app.use("/api/user", userRoutes);
 app.use("/api/roles", rolesRoutes);
 app.use("/api/approval", approvalRoutes);
 app.use("/api/manage-services", manageServiceRoutes);
